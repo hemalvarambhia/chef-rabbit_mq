9335d888bccc786f9fc30b1403a4b24d8ba6e7ba
Hemal <h.n.varambhia@gmail.com>
2016-01-07 12:28:29 +0000

    Check logrotate is installed

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe "installing RabbitMQ" do
+  describe 'installing dependencies' do
+    describe package('logrotate') do
+      it { should be_installed }
+    end
+  end
+
   describe package('rabbitmq-server') do
     it { should be_installed }
   end
