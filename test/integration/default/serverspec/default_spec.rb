fd810d0dba4baaffa1463a00d86128b898bd37ae
Hemal <h.n.varambhia@gmail.com>
2016-01-07 10:32:44 +0000

    Corrected description

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
-describe "installing ruby" do
+describe "installing RabbitMQ" do
   describe package('rabbitmq-server') do
-    it {
-      should be_installed
-    }
+    it { should be_installed }
   end
 end
