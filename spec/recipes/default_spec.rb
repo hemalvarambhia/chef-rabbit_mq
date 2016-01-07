84df2399254c2b618fa354fce50014e7515915c9
Hemal <h.n.varambhia@gmail.com>
2016-01-07 12:28:54 +0000

    Consistent formatting

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -2,7 +2,7 @@ require 'chefspec'
 
 describe "chef-rabbit_mq::default" do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
-  
+
   it 'installs RabbitMQ server' do
     expect(chef_run).to install_package 'rabbitmq-server'
   end
